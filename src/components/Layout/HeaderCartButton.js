2cb7848aadc74b0a35dbc97dbc7cde59858757e3
Hanqing Liu <lhq3361951@gmail.com>
2022-01-06 22:02:26 -0500

    used useContext and useRef to add items to cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,25 @@
-import CartIcon from "../Cart/CartIcon";
+import {useContext} from "react";
+
+import CartContext from "../../store/CartContext";
+//import CartIcon from "../Cart/CartIcon";
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItem = cartCtx.item.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  },0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
       <AiOutlineShoppingCart />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItem}</span>
     </button>
   );
 };
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,22 @@
+import { useContext } from 'react';
+
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from '../../../store/CartContext';
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.meal.price.toFixed(2)}`;
+
+  const addToCartHandler = amount => {
+    cartCtx.addItem({
+      id: props.meal.id,
+      name: props.meal.name,
+      amount: amount,
+      price: props.meal.price
+    })
+  };
   return (
     <li className={classes.meal}>
       <div>
@@ -11,7 +25,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-      <MealItemForm id={props.id}/>
+      <MealItemForm onAddToCart={addToCartHandler}/>
       </div>
     </li>
   );
