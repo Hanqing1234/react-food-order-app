bea739e225677e964524b30fa317cc452ee6aaee
Hanqing Liu <lhq3361951@gmail.com>
2022-01-07 21:47:47 -0500

    added animation to cart component by useEffect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 
 import CartContext from "../../store/CartContext";
 //import CartIcon from "../Cart/CartIcon";
@@ -7,14 +7,33 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const numberOfCartItem = cartCtx.item.reduce((curNumber, item) => {
     return curNumber + item.amount
   },0);
 
+
+  const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if(cartCtx.item.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
       <AiOutlineShoppingCart />
       </span>
